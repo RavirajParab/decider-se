9b3828a42dffc48946de573ac2f9a2338ef1eaff
raviraj.parab <raviraj.parab@infosys.com>
2020-04-07 15:26:40 +0530

    done

--- RSIBet20n30/index.js
+++ RSIBet20n30/index.js
@@ -1,4 +1,4 @@
-const util = require("./utilities");
+const util = require("../utilities");
 module.exports = async function (context, req) {
   const RSIdata = await util.getRSI(
     "https://sas.indiatimes.com/TechnicalsClient/getRSI.htm?crossovertype=RSI_BETWEEN_20_AND_30&pagesize=500"
